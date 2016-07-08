318aa44f9e81947b5aabca36820c11ad8d065753
chriswebb09 <chris.webb5249@gmail.com>
2016-07-08 10:53:30 -0400

    new code

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -1,22 +1,42 @@
-func brokenFirstItem(items: [String]) -> String {
-    return items[0]
-}
-
-var fruits = ["apple", "banana", "grape"]
-brokenFirstItem(fruits)  // Works!
-fruits = []
-// Uncomment the next line to see the problem.
-// You will get an "index out of range" error.
-//brokenFirstItem(fruits)  // Breaks :-(
-
-
-
 func firstItem(items: [String]) -> String? {
     if items.isEmpty {
         return nil
     }
     return items[0]
 }
+var fruits = ["apple", "banana", "grape"]
+print("There are no fruits!")
+
+func brokenFirstItem(items: [String]) -> String?  {
+    if let fruit = firstItem(fruits) {
+    print("The first fruit is \(fruit)")
+    }
+    return firstItem(fruits)
+}
+
+
+
+
+brokenFirstItem(fruits)  // Works!
+fruits = []
+
+if let fruit = firstItem(fruits) {
+    print("The first fruit is \(fruit)")
+} else {
+    print("There are no fruits!")
+}
+// Uncomment the next line to see the problem.
+// You will get an "index out of range" error.
+brokenFirstItem(fruits)  // Breaks :-(
+
+
+
+//func firstItem(items: [String]) -> String? {
+//    if items.isEmpty {
+//        return nil
+//    }
+//    return items[0]
+//}
 
 fruits = ["apple", "banana", "grape"]
 firstItem(fruits)
